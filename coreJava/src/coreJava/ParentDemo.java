7c31700d99ee24b7d19080ebd30642d23b0dadad
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-03-14 09:47:09 +0700

    Inheritance

---
+++ coreJava/src/coreJava/ParentDemo.java
@@ -0,0 +1,22 @@
+package coreJava;
+
+public class ParentDemo {
+
+		String name= "Rahul";
+
+		public void parentDemo()
+		{
+			System.out.println("Parent class construtor");
+		}
+		
+		public void getData()
+		{
+			System.out.println(" I am parent class");
+		}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
