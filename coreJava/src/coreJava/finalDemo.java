59a15f8e273d944a05c698eed9a530fe217e4949
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-03-20 21:04:01 +0700

    "final" keywords

---
+++ coreJava/src/coreJava/finalDemo.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+public class finalDemo {
+
+//	final void getData() {
+//
+//	 }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int i =0;
+	}
+
+}
